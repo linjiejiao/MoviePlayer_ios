b227882c56f75325d502927d6da6f968d29c774f
linjiejiao <hpckljj@gmail.com>
2016-07-27 10:23:08 +0800

    project base

---
+++ MoviePlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MoviePlayer
+//
+//  Created by liangjiajian_mac on 16/7/27.
+//  Copyright © 2016年 cn.ljj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MoviePlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MoviePlayer
+//
+//  Created by liangjiajian_mac on 16/7/27.
+//  Copyright © 2016年 cn.ljj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
